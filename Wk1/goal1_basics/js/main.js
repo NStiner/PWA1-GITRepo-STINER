0d595ee1899f2025fd8df29e2c1ff19dfe50813b
NStiner <ndstiner@fullsail.edu>
2015-03-04 19:20:34 -0500

    followed along with Functions Part 1
    Completed

--- Wk1/goal1_basics/js/main.js
+++ Wk1/goal1_basics/js/main.js
@@ -535,7 +535,18 @@ console.log('------Functions ----------');
      - type the below as an example
         functionName();
  *****************************
+*/
 
+    var my ctr = 1;
+
+    var myCounter = function(){
+        var testVar = 0;
+        myctr++;
+        console.log('counter = ', myctr);
+    };
+
+    myCounter();
+    myCounter();
 
 
 
@@ -574,7 +585,14 @@ console.log('------Functions ----------');
     - items inside our function we call on these variables.. such as:
  */
 
+        var value1 = 1;
+        var value2 = 2;
 
+        var myFn = function(var1, var2) {
+            console.log('function number: ', var1 + var2 );
+        };
+
+        myFn(value1, value2);
 
 
 /*******************************************
