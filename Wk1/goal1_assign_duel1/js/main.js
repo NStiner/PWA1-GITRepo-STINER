b7aab1faf499ed8e6f0946988a1105ee8291d4dc
NStiner <ndstiner@fullsail.edu>
2015-03-05 23:44:02 -0500

    Starting Duel 1
    Created js folder with main.js inside. Started following along with the
    video for Duel 1 (because I’m new to programming). Linked js file to
    html, created variables for game.

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,26 @@
+/*
+ Nicole Stiner
+ 03/05/15
+ Assignment: Duel 1
+ */
+
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player names
+    var playerOneName = "Kabal";
+    var playerTwoName = "Kratos";
+
+    //player damage
+    var playerOneDamage = "20";
+    var playerTwoDamage = "20";
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //round
+    var round =1;
+})();
