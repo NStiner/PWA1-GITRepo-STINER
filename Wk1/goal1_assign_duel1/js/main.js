adf4bdf39a8d7d147514315ca6c7423ca0d66c61
NStiner <ndstiner@fullsail.edu>
2015-03-06 00:19:30 -0500

    added code to functions
    added code and variables to the fight and winnerCheck functions.
    console.logged results, works fine.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -28,14 +28,39 @@
     function fight(){
         console.log('in the fight function');
 
+        alert(playerOneName + ":" + " " + playerOneHealth + " " + "*START*" + " " + playerTwoName + ":" + " " + playerTwoHealth);
+
         for (var i=0; i<10; i++){
-            console.log(i);
+            var minDamage1 = playerOneDamage * .5;
+            var minDamage2 = playerTwoDamage * .5;
+            var f1 = Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
+
+            //inflicting damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneName + ":" + " " + playerOneHealth + " " + " " + playerTwoName + ":" + " " + playerTwoHealth);
+         //random formula is Math.floor(Math.random() * (max - min) + min);
+
+
         };
-        /*winnerCheck()*/
+        var results = winnerCheck();
+        console.log(results);
     };
 
     function winnerCheck(){
-        //code will go here
+        console.log("in winnerCheck FN");
+        var result = "no winner";
+
+        if(playerOneHealth < 1 && playerTwoHealth <1){
+            result = "You both die"
+        }else if(playerOneHealth < 1){
+            result = playerTwoName + " " + "WINS!!!"
+        }else if(playerTwoHealth < 1){
+            result = playerOneName + " " + "WINS!!!"
+        };
+        return result;
     };
 
 /**** The program gets started below ****/
