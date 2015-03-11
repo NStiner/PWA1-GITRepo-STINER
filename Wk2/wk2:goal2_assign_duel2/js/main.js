968800b39cc3ea4675261c6e52ab10196fbb4542
NStiner <ndstiner@fullsail.edu>
2015-03-11 16:20:59 -0400

    copied files over into week 2

---
+++ Wk2/wk2:goal2_assign_duel2/js/main.js
@@ -0,0 +1,128 @@
+/*
+ Nicole Stiner
+ 03/05/15
+ Assignment: Duel 1
+ */
+
+/*** Pseudocode ***/
+/*
+DISPLAY player names, player healths, starting round
+
+
+/*** player One health > 1 , player two health < 1 ***/
+/*
+PSEUDOCODE:
+    DISPLAY  player one health > 1 , player two health < 1
+    CALCULATE winner
+    RESET counters
+    START new fight
+*/
+
+/*** player one health < 1 , player two health > 1 ***/
+/*
+PSEUDOCODE:
+    DISPLAY player one health > 1 , player two health < 1
+    CALCULATE winner
+    RESET counters
+    START new fight
+*/
+
+/*** both players health < 1 ***/
+/*
+PSEUDOCODE:
+    DISPLAY play one health < 1 , player two health < 1
+    RESET counters
+    START new fight
+*/
+
+/*** both players have health > 1 ***/
+/*
+PSEUDOCODE:
+    DISPLAY play one health > 1 , player two health > 1
+    READ round
+    CALCULATE life
+    RETURN values
+    STORE life
+    START next round
+*/
+
+/*** max rounds reached - round 10 and both players health > 1 ***/
+/*
+PSEUDOCODE:
+    READ max rounds
+    READ player one health > 1 && player two health > 1
+    CALCULATE life
+    RETURN values
+    STORE life
+    START next round
+ */
+
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player names
+    var playerOneName = "Kabal";
+    var playerTwoName = "Kratos";
+
+    //player damage
+    var playerOneDamage = 20;
+    var playerTwoDamage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //round
+    var round = 0;
+
+    //functions
+    function fight(){
+        alert(playerOneName + ":" + " " + playerOneHealth + " " + "*START*" + " " + playerTwoName + ":" + " " + playerTwoHealth);
+
+        for (var i=0; i<10; i++) {
+            var minDamage1 = playerOneDamage * .5;
+            var minDamage2 = playerTwoDamage * .5;
+            var f1 = Math.floor(Math.random() * (playerOneDamage - minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random() * (playerTwoDamage - minDamage2) + minDamage2);
+
+            //inflicting damage
+            playerOneHealth -= f1;
+            playerTwoHealth -= f2;
+
+            console.log(playerOneName + ":" + " " + playerOneHealth + " " + " " + playerTwoName + ":" + " " + playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if (results === "no winner") {
+                round++;
+                alert(playerOneName + ":" + " " + playerOneHealth + " " + "*ROUND" + " " + round + " " + "OVER*" + " " + playerTwoName + ":" + " " + playerTwoHealth);
+            } else {
+                alert(results);
+                break;
+            };
+        };
+    };
+
+    function winnerCheck(){
+        console.log("in winnerCheck FN");
+        var result = "no winner";
+
+        if(playerOneHealth < 1 && playerTwoHealth < 1){
+            result = "You both die"
+        }else if(playerOneHealth < 1){
+            result = playerTwoName + " " + "WINS!!!"
+        }else if(playerTwoHealth < 1){
+            result = playerOneName + " " + "WINS!!!"
+        };
+        return result;
+    };
+
+/**** The program gets started below ****/
+    console.log('program starts');
+    fight();
+
+
+})();
