f98f0d6616c2c5edd8bd0f503144dc6d5ab43d0c
NStiner <ndstiner@fullsail.edu>
2015-03-16 13:59:43 -0400

    changed files to week 3
    copied files from week two and switched them over to week 3.

---
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -0,0 +1,21 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Nicole Stiner
+ * Date: 03/16/15
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+(function ()
+
+    var dom = {
+        input: document.querySelector("#input"),
+        output: document.querySelector("#output"),
+        button: document.querySelector("button")
+    };
+
+
+
+})();
