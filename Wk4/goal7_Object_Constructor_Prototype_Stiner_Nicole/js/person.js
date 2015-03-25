af786e23ed51c52da331650297cae0dc73317480
NStiner <ndstiner@fullsail.edu>
2015-03-25 09:53:02 -0400

    Finished goal 7
    moved files into pwa1 file. submitted

---
+++ Wk4/goal7_Object_Constructor_Prototype_Stiner_Nicole/js/person.js
@@ -0,0 +1,18 @@
+/*
+ Nicole Stiner
+ PWA1: Goal 7:  Course Material 7 (more.Objects - Constructors - Prototypes)
+ 03/24/2015
+ ===================================================================
+ PSUEDOCODE:
+
+1. Start the function by making persons globally visable.
+2. Adding four jobs and three actions for "persons" ending the first function
+3. Starting second function creating person, action, job row
+4. Set random job for person
+5. Set random action for person
+6. Arranging the actions to change through them randomly ever second
+ */
+
+(function(){
+
+})(); //end of wrapper
