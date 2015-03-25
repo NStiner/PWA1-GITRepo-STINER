cbcdb138ca64636717936f3bfa7424dd62a8cf9f
NStiner <ndstiner@fullsail.edu>
2015-03-25 10:01:29 -0400

    Created variables and names
    Created the function wrapper and started creating variables needed
    throughout file and chose the 5 names for the “persons” array

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -5,3 +5,13 @@
  * Class: PWA
  * Goal: Goal7
  */
+
+(function(){
+    //Created all variables
+    var numOfPeople =3;
+    var people = [];
+    var names = ["Nicole","Travis","Holly","James","Chelsea"];
+    var interval;
+
+    
+})();
