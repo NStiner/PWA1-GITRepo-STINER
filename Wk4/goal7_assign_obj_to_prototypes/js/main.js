9a3c0bfdddb8adb0fbd4322a42235c30184a8ad2
NStiner <ndstiner@fullsail.edu>
2015-03-25 10:02:47 -0400

    created loop for names
    to be randomly selected - also created new person variable

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -13,5 +13,21 @@
     var names = ["Nicole","Travis","Holly","James","Chelsea"];
     var interval;
 
-    
+    //Created loop to randomly select persons names and display them
+    for(var i = 0; i<numOfPeople;i++){
+
+        //make a number between 1-5
+        var personIndex = Math.floor(Math.random()*names.length);
+
+        //Selecting new person
+        var person = new Person(names[personIndex],i+1);
+
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+
+        people.push(person);
+        names.splice(personIndex,1);
+    }
+
+
 })();
